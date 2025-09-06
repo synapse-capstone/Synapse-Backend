60218a365d869cc292c41ba9dbb605dd4686baf6
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-09-06 23:28:57 +0900

    feat: 개발 환경 세팅

---
+++ src/main/java/com/synapse/synapse/SynapseApplication.java
@@ -0,0 +1,13 @@
+package com.synapse.synapse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synapse/synapse/SynapseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synapse.synapse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
