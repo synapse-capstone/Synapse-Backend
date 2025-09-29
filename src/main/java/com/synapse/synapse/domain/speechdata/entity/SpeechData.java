542dd331776ab9a547029f313c2186e58b7af4f2
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-09-30 00:01:10 +0900

    #1 feat: SpeechData 엔티티 작업

---
+++ src/main/java/com/synapse/synapse/domain/speechdata/entity/SpeechData.java
@@ -0,0 +1,28 @@
+package com.synapse.synapse.domain.speechdata.entity;
+
+import com.synapse.synapse.domain.user.entity.User;
+import com.synapse.synapse.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name="speech_data")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SpeechData extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id",unique=true)
+    private User user;
+
+    @Lob
+    @Column(nullable = false)
+    private String speechText;
+
+}
