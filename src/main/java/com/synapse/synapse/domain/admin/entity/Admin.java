f8c66d753c10b64e1c97a020c606f0dd37bfe8af
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-09-30 00:01:39 +0900

    #1 feat: Admin 엔티티 작업

---
+++ src/main/java/com/synapse/synapse/domain/admin/entity/Admin.java
@@ -0,0 +1,37 @@
+package com.synapse.synapse.domain.admin.entity;
+
+import com.synapse.synapse.domain.user.model.UserType;
+import com.synapse.synapse.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name="admin")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class  Admin extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 25)
+    private UserType userType = UserType.ADMIN;
+
+    @Column(nullable = false, length = 50 )
+    @Email
+    private String email;
+
+    @Size(min = 8, max = 64, message = "비밀번호는 8자 이상 64자 이하이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*()\\-_=+\\[\\]{};:'\",.<>/?]).+$",
+            message = "비밀번호는 최소 1개의 숫자와 1개의 특수문자를 포함해야 합니다."
+    )
+    private String password;
+
+}
